1d6b69090d64a4bd6ff771e98ccfca19a595669c
mpcahn <mpcahn@gmail.com>
2018-10-18 14:53:33 -0600

    Add classAndMethods project

---
+++ classAndMethods/classAndMethods/IntMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAndMethods
+{
+    class IntMethods
+    {
+        public int Add5(int i)
+        {
+            return i + 5;
+        }
+
+        public int Subtract5(int i)
+        {
+            return i - 5;
+        }
+
+        public int Mult5(int i)
+        {
+            return i * 5;
+        }
+    }
+}
---
+++ classAndMethods/classAndMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("Enter an Integer:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //3. Call each method in turn, passing the user input to the method.Display the returned integer to the screen.
+            IntMethods IntMethod = new IntMethods();
+
+            Console.WriteLine(num + " + 5 = " + IntMethod.Add5(num));
+            Console.WriteLine(num + " - 5 = " + IntMethod.Subtract5(num));
+            Console.WriteLine(num + " * 5 = " + IntMethod.Mult5(num));
+            Console.ReadLine();
+        }
+    }
+}
