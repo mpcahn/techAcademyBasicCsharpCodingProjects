ea8cb8bad15f0fa68595f9605772547312256c20
mpcahn <mpcahn@gmail.com>
2018-10-23 23:09:16 -0600

    Add StructExercise Project

---
+++ StructExercise/StructExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise
+{
+    //IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+    //1.Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+    struct Number
+    {
+        public decimal Amount{ get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+            Number number = new Number();
+            number.Amount = 4.333m;
+            //3. Print this amount to the console.
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
