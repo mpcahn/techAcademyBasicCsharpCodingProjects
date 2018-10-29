e363524dcf0a226cdfc58f2642299bbc75028ae1
mpcahn <mpcahn@gmail.com>
2018-10-28 18:03:13 -0600

    Add AllTogetherExercise Project.

---
+++ AllTogetherExercise/AllTogetherExercise/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllTogetherExercise
+{
+    public class Class1
+    {
+        //1. Create a const variable
+        const string constant = "Unchangable";
+
+        //2. Create a variable using the keyword "var".
+        var newDictionary = new Dictionary<string, string>();
+
+        //3. Chain two constructors together.
+        Player player = new Player("Matt");
+    }
+}
---
+++ AllTogetherExercise/AllTogetherExercise/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllTogetherExercise
+{
+    class Player
+    {
+        public Player(string name) : this(name, 100) { }
+        public Player(string name, int begBalance)
+        {
+            Balance = begBalance;
+            Name = name;
+        }
+        
+        public int Balance { get; set; }
+        public string Name { get; set; }        
+    }
+}
